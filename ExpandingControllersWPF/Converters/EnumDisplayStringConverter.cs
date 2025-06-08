4747ceea8932f550fe2ed3cd35d0ed0986ace471
Artem Fedotov <kebabggbab@gmail.com>
2025-06-08 14:46:01 +0700

    Добавленны два класса конвертера: 1) FlagsEnumBoolConverter - проверяет наличие значения в битом поле. 2) EnumDisplayStringConverter - возвращает свойство Name артибута DisplayAttribute поля перечисления.

---
+++ ExpandingControllersWPF/Converters/EnumDisplayStringConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ExpandingControllersWPF.Converters
+{
+	public class EnumDisplayStringConverter : IValueConverter
+	{
+		public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return string.Empty;
+			}
+
+			Enum field = (Enum)parameter;
+			object[] attributes = field.GetType().GetCustomAttributes(false);
+
+			foreach (object attribute in attributes)
+			{
+				if (attribute is DisplayAttribute display)
+				{
+					return display.Name;
+				}
+			}
+
+			return field.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ ExpandingControllersWPF/Converters/FlagsEnumBoolConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ExpandingControllersWPF.Converters
+{
+	public class FlagsEnumBoolConverter : IValueConverter
+	{
+		private int _value;
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null || parameter == null)
+			{
+				return false;
+			}
+
+			int mask = (int)parameter;
+			_value = (int)value;
+
+			return (mask & _value) != 0;
+		}
+
+		public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null || parameter == null)
+			{
+				return 0;
+			}
+
+			_value ^= (int)parameter;
+			return Enum.Parse(targetType, _value.ToString());
+		}
+	}
+}
